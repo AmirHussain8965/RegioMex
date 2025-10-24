cec2356bd0733f63f12057715766ff7444a2e801
Amir Hussain <amirhussain8965@gmail.com>
2025-10-24 16:06:00 +0500

    Lattest

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,18 @@ document.querySelectorAll(".navbar ul li a").forEach(link => {
         this.classList.add("active");
     });
 });
+
+
+const toggle = document.getElementById('lang_toggle');
+const fr = document.getElementById('lang_fr');
+const de = document.getElementById('lang_de');
+
+toggle.addEventListener('change', () => {
+  if (toggle.checked) {
+    fr.style.opacity = "0.5";
+    de.style.opacity = "1";
+  } else {
+    fr.style.opacity = "1";
+    de.style.opacity = "0.5";
+  }
+});
